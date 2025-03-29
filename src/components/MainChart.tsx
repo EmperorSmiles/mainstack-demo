13f0ac24b79ff63987c48f2060c545d9b4946cb6
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-03-29 19:11:13 +0100

    Updating transaction block

--- src/components/MainChart.tsx
+++ src/components/MainChart.tsx
@@ -14,17 +14,17 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
-
-interface Transaction {
-  date: string;
-  amount: number;
-  [key: string]: unknown; // For any other properties that might exist
-}
 
 interface DataType {
   date: string;
   amount: number;
+}
+
+interface MainChartProps {
+  chartData: DataType[];
+  balance: number | null;
+  loading: boolean;
+  walletData: Record<string, number> | null;
 }
 
 // Chart config
@@ -35,51 +35,12 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export default function MainChart() {
-  const [chartData, setChartData] = useState<DataType[]>([]);
-  const [balance, setBalance] = useState<number | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [walletRes, txnRes] = await Promise.all([
-          fetch("https://fe-task-api.mainstack.io/wallet").then((res) =>
-            res.json()
-          ),
-          fetch("https://fe-task-api.mainstack.io/transactions").then((res) =>
-            res.json()
-          ),
-        ]);
-
-        console.log("Wallet Data:", walletRes);
-        console.log("Transaction Data:", txnRes);
-
-        setBalance(walletRes.balance);
-
-        // Convert transaction amounts into chart data
-        const formattedData = txnRes
-          .filter((txn: Transaction) => txn.date)
-          .map((txn: Transaction) => ({
-            date: new Date(txn.date).toISOString().split("T")[0],
-            amount: txn.amount,
-          }))
-          .sort(
-            (a: DataType, b: DataType) =>
-              new Date(a.date).getTime() - new Date(b.date).getTime()
-          );
-
-        setChartData(formattedData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+export default function MainChart({
+  chartData,
+  balance,
+  loading,
+  walletData,
+}: MainChartProps) {
   const formatDate = (dateStr: string) => {
     if (!dateStr) return "";
     return new Intl.DateTimeFormat("en-US", {
@@ -97,62 +58,83 @@ export default function MainChart() {
   };
 
   return (
-    <Card className="w-auto h-72 mx-4">
-      <CardHeader>
-        <div className="flex items-center justify-between">
-          <div>
-            <CardTitle>Available Balance</CardTitle>
-            <CardDescription
-              className={
-                loading
-                  ? "animate-pulse text-4xl font-bold"
-                  : "text-4xl font-bold"
-              }
-            >
-              {loading
-                ? "Loading..."
-                : balance !== null
-                ? `$${balance}`
-                : "N/A"}
-            </CardDescription>
+    <div className="grid grid-cols-1 md:grid-cols-10 gap-4 md:gap-6 w-full h-full">
+      <Card className="w-full h-72 md:col-span-7 mb-6 md:mb-0">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <div>
+              <CardTitle>Available Balance</CardTitle>
+              <CardDescription
+                className={
+                  loading
+                    ? "animate-pulse text-4xl font-bold"
+                    : "text-4xl font-bold"
+                }
+              >
+                {loading
+                  ? "Loading..."
+                  : balance !== null
+                  ? `$${balance}`
+                  : "N/A"}
+              </CardDescription>
+            </div>
+            <Button className="rounded-full">Withdraw</Button>
           </div>
-          <Button className="rounded-full">Withdraw</Button>
-        </div>
-      </CardHeader>
-      <CardContent className=" px-0 pb-0">
-        <ChartContainer config={chartConfig} className="h-full">
-          <LineChart
-            accessibilityLayer
-            data={chartData}
-            margin={{ left: 40, right: 40, top: 5, bottom: 25 }}
-          >
-            <CartesianGrid />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={formatDate}
-              ticks={getTickValues()}
-              interval={0}
-              minTickGap={0}
-              className="text-md text-muted-foreground"
-            />
-            {/* <YAxis /> */}
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Line
-              dataKey="amount"
-              type="natural"
-              stroke="var(--color-chart-5)"
-              strokeWidth={2}
-              dot={{ r: 3 }}
-            />
-          </LineChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+        </CardHeader>
+        <CardContent className="px-0 pb-0">
+          <ChartContainer config={chartConfig} className="h-full">
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{ left: 40, right: 40, top: 5, bottom: 25 }}
+            >
+              <CartesianGrid />
+              <XAxis
+                dataKey="date"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={formatDate}
+                ticks={getTickValues()}
+                interval={0}
+                minTickGap={0}
+                className="text-md text-muted-foreground"
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey="amount"
+                type="natural"
+                stroke="var(--color-chart-5)"
+                strokeWidth={2}
+                dot={{ r: 3 }}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+      <Card className="w-full h-72 md:col-span-3 rounded-0 shadow-none">
+        <CardContent>
+          {walletData ? (
+            <ul className="space-y-2">
+              {Object.entries(walletData).map(([key, value]) => (
+                <li key={key} className="flex flex-col justify-between">
+                  <span className="capitalize text-muted-foreground">
+                    {key.replace(/_/g, " ")}:
+                  </span>
+                  <span className="font-bold text-xl md:text-2xl">
+                    ${value}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </CardContent>
+      </Card>
+    </div>
   );
 }
