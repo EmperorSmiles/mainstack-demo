da63881e32eb7da22fd8042057ff82d6fb94737c
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-03-29 02:26:04 +0100

    Moved User Profile to a separate comp

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,22 @@
+import { BsChatLeftText } from "react-icons/bs";
+import { FaRegBell } from "react-icons/fa6";
+import { FiMenu } from "react-icons/fi";
+
+const UserProfile = () => {
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <FaRegBell className="text-black/50" />
+      <BsChatLeftText className="text-black/60" />
+      <div className="flex items-center justify-between bg-gray-100 p-2 rounded-3xl w-22 gap-2 hover:cursor-pointer">
+        {/* Large Circle with Initials */}
+        <div className="w-8 h-8 flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 text-white text-sm font-bold rounded-full">
+          OJ
+        </div>
+        {/* Hamburger Icon */}
+        <FiMenu className="text-gray-600 w-6 h-6" />
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
