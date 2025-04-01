bd06408b88ac7a437504092e8ae3a6cc6883bfc4
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-01 19:32:02 +0100

    byg fixes and UX tweaks

---
+++ src/app/apps/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Apps = () => {
+  return (
+    <div className="flex justify-center items-center align-middle h-screen">
+      Hello from Apps
+    </div>
+  );
+};
+
+export default Apps;
