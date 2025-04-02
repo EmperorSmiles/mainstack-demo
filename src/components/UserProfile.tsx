f572544d0293fa8177c0662f3211e6e82d176bbc
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-02 02:07:53 +0100

    helper function to get initials

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,16 +1,35 @@
+import { User } from "@/types/type";
 import { BsChatLeftText } from "react-icons/bs";
 import { FaRegBell } from "react-icons/fa6";
 import { FiMenu } from "react-icons/fi";
 
-const UserProfile = () => {
+interface UserProp {
+  user: User | null;
+  onClick: () => void;
+}
+
+const UserProfile: React.FC<UserProp> = ({ user, onClick }) => {
+  console.log(user);
+
+  const getInitials = () => {
+    if (!user) return "JD";
+
+    const firstInitial = user.first_name?.at(0) || "";
+    const lastInitial = user.last_name?.at(0) || "";
+
+    return firstInitial + lastInitial || "JD";
+  };
   return (
-    <div className="flex justify-center items-center gap-6">
+    <div className="flex justify-center items-center md:gap-6 gap-2">
       <FaRegBell className="text-black/50" />
       <BsChatLeftText className="text-black/60" />
-      <div className="flex items-center justify-between bg-gray-100 p-2 rounded-3xl w-22 gap-2 hover:cursor-pointer">
+      <div
+        className="flex items-center justify-between bg-gray-100 p-2 rounded-3xl w-22  gap-2 hover:cursor-pointer"
+        onClick={onClick}
+      >
         {/* Large Circle with Initials */}
-        <div className="w-8 h-8 flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 text-white text-sm font-bold rounded-full">
-          OJ
+        <div className="w-8 h-8 flex items-center justify-center bg-gradient-to-b from-gray-900 to-gray-700 text-white md:text-sm text-xs font-bold rounded-full">
+          {getInitials()}
         </div>
         {/* Hamburger Icon */}
         <FiMenu className="text-gray-600 w-6 h-6" />
