1b378b1c1ddb2e53539254dc7a8d619bcec0cb1a
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-03 11:11:25 +0100

    Small styling

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,9 @@
+const Analytics = () => {
+  return (
+    <div className="flex justify-center items-center align-middle h-screen">
+      <h1 className="text-2xl font-bold">Hello from Analytics</h1>
+    </div>
+  );
+};
+
+export default Analytics;
