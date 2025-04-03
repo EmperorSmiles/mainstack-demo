a2d172cf7c2abf6ae675a753f7b0692e8c58c8fe
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-03 11:11:18 +0100

    Small styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Hello, Mainstack</h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-2xl font-bold">Hello, Mainstack</h1>
+      <Button onClick={() => router.push("/revenue")}>Go to Revenue</Button>
     </div>
   );
 }
