f5652d589669126b96c38a12ad66ab19099ccfff
Favour Smiles Adejoh <iamfavoursmiles@gmail.com>
2025-04-03 10:54:33 +0100

    not much time to finish testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from "jest";
+// import type { Config } from "jest";
 
-const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.svg$": "<rootDir>/__mocks__/fileMock.js",
-  },
-  transform: {
-    "^.+\\.(ts|tsx)$": [
-      "ts-jest",
-      {
-        tsconfig: "<rootDir>/tsconfig.jest.json",
-      },
-    ],
-  },
-};
+// const config: Config = {
+//   preset: "ts-jest",
+//   testEnvironment: "jest-environment-jsdom",
+//   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+//   moduleNameMapper: {
+//     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+//     "\\.svg$": "<rootDir>/__mocks__/svgMock.ts",
+//   },
+//   transform: {
+//     "^.+\\.(ts|tsx)$": [
+//       "ts-jest",
+//       {
+//         tsconfig: "<rootDir>/tsconfig.jest.json",
+//       },
+//     ],
+//   },
+// };
 
-export default config;
+// export default config;
